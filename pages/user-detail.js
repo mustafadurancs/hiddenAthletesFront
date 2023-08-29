3333bd96bbe79f3759a91d4ac7adbe3b2a50e49c
Mustafa Duran <mduranmustafa@gmail.com>
2023-08-28 22:06:15 -0500

    Questionnaire evaluation

--- pages/user-detail.js
+++ pages/user-detail.js
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 import configData from '../jsconfig.json';
+import PageBanner from "@/components/Common/PageBanner";
 
 function UserDetail() {
     const router = useRouter();
@@ -22,13 +23,29 @@ function UserDetail() {
     }
 
     return (
-        <div>
+        <>
+        <PageBanner pageTitle="QUESTIONNAIRE EVALUATION " />
+        <div className="container">
             <h1>{user.firstName} {user.lastName}</h1>
-            <p>Email: {user.email}</p>
-            <p>City: {user.city}</p>
-            <p>State: {user.state}</p>
-            <p>GPA: {user.gpa}</p>
+            <ul>
+                <li><strong>Email</strong>: <input type="text" value={user.email} disabled  style={{ width: '100%', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}/></li>
+            <li>City: {user.city}</li>
+            <li>State: {user.state}</li>
+            <li>GPA: {user.gpa}</li>
+            <li>SAT: {user.sat}</li>
+            <li>Home Address: {user.homeAddress}</li>
+            <li>Academic Info: {user.academicInfo}</li>
+            <li>General Info: {user.generalInfo}</li>
+            <li>2/4 Year: {user.twoFourYear}</li>
+            <li>ACT: {user.act}</li>
+            </ul>
+            <textarea defaultValue="Feedback here..." style={{ width: '100%'}}>
+
+            </textarea>
+            <input type="button" value="SUBMIT"/>
+
         </div>
+        </>
     );
 }
 
